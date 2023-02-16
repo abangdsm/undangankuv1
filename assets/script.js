dc4cafbf09141b635ffe6b3a79ed41f54ad95c3c
abangdsm <id.reversal@gmail.com>
2023-02-16 15:50:58 +0700

    Menambahkan gambar dan modifikasi CSS

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,14 @@
-const prmtr = new URLSearchParams(window.location.search);
-const namaUndangan = prmtr.get('to');
+// const prmtr = new URLSearchParams(window.location.search);
+// const namaUndangan = prmtr.get('to');
 
-const p = document.createElement('p');
-const node = document.createTextNode(namaUndangan);
-p.appendChild(node);
+// const p = document.createElement('p');
+// const node = document.createTextNode(namaUndangan);
+// p.appendChild(node);
 
-const tamu = document.getElementsByClassName('cardname')[0];
-tamu.appendChild(p);
+// const tamu = document.getElementsByClassName('cardname')[0];
+// tamu.appendChild(p);
 
-let cl = tamu.getElementsByTagName('p')[0];
-cl.setAttribute('class', 'tamu');
+// let cl = tamu.getElementsByTagName('p')[0];
+// cl.setAttribute('class', 'tamu');
+
+console.log('halo');
